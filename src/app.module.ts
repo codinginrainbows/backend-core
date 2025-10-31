1ba3cb49073d43cf13d29720e632518db40bc60e
gabriel garcia <gabrielgarcia.contactme@gmail.com>
2025-10-31 17:07:28 -0300

    feat: add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
+import { CompaniesModule } from './companies/companies.module';
+import { AuthModule } from './auth/auth.module';
+import { InvitationsModule } from './invitations/invitations.module';
+import { MailerModule } from './mailer/mailer.module';
+import { WorkersModule } from './workers/workers.module';
+import { CacheModule } from './cache/cache.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    DatabaseModule,
+    CompaniesModule,
+    AuthModule,
+    InvitationsModule,
+    MailerModule,
+    WorkersModule,
+    CacheModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
